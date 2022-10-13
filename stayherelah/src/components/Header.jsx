7c2932f197162b4529e75909e3d6ea9a8bcfeb46
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-10-13 15:52:43 +0800

    Updated header component

--- stayherelah/src/components/Header.jsx
+++ stayherelah/src/components/Header.jsx
@@ -3,15 +3,42 @@ import styled from "styled-components";
 import React from "react";
 
 const Container = styled.div`
-  background-color: red;
-  margin: 0;
+  border-color: black;
+  border-style: solid;
   display: flex;
   align-items: center;
-  justify-content: center;
+  gap: 10rem;
+  height: 3rem;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+
+  border: solid;
+`;
+
+const Left = styled.div`
+  border: solid;
+  margin-left: 3rem;
+`;
+
+const Centre = styled.div`
+  display: flex;
+  border: solid;
+`;
+
+const MenuItem = styled.div``;
+
 const header = () => {
-  return <Container>header</Container>;
+  return (
+    <Container>
+      <Left>StayHereLah!</Left>
+      <Centre>
+        <MenuItem>Explore</MenuItem>
+        <MenuItem>BTO projects</MenuItem>
+      </Centre>
+    </Container>
+  );
 };
 
 export default header;
