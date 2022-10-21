04e9509b1788d38148911e6633160411e8e08c30
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-10-21 21:39:35 +0800

    Added some routing

--- stayherelah/src/App.js
+++ stayherelah/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home";
 import Property from "./pages/Property";
+import Estimate from "./pages/Estimate";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,7 +12,22 @@ import {
 function App() {
   return (
     <Router>
-      <Property />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
+        <Route path="/bto/mature" element={<Property />}>
+          <Route path=":id" element={<Property />} />
+        </Route>
+
+        <Route path="/bto/nonmature" element={<Property />}>
+          <Route path=":id" element={<Property />} />
+        </Route>
+
+        <Route
+          path="/bto/nonmature/:id/estimate"
+          element={<Estimate />}
+        ></Route>
+      </Routes>
     </Router>
   );
 }
