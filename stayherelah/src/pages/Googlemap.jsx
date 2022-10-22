593b9ce6eb3e88fae50f7c1acf5de83ce277256f
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-10-22 23:03:16 +0800

    updated Googlepage

--- stayherelah/src/pages/Googlemap.jsx
+++ stayherelah/src/pages/Googlemap.jsx
@@ -1,6 +1,9 @@
 import Header from "../components/Header";
 import background from "../images/home/houses.jpg";
+import Map from "../components/Map_Explore"
 import styled from "styled-components";
+import {useLocation} from "react-router-dom";
+
 import React from "react";
 
 const Container = styled.div`
@@ -12,10 +15,15 @@ const Container = styled.div`
 `;
 
 const Googlemap = () => {
+
+  const location = useLocation()
+  let data = location.state.from;
+
   return (
     <>
       <Container>
         <Header></Header>
+        <Map location = {data}></Map>
       </Container>
     </>
   );
