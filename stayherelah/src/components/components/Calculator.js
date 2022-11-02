271234d679f21f2226877da48f38d1c95c1f2ca2
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-11-02 17:26:47 +0800

    updated the value

--- stayherelah/src/components/components/Calculator.js
+++ stayherelah/src/components/components/Calculator.js
@@ -1,53 +1,59 @@
-import React, {useState} from 'react';
-import {HeaderFigures} from './HeaderFigures';
-import {CalculatorControls} from './CalculatorControls';
-import {PaymentGraph} from './PaymentGraph';
-import {PaymentTable} from './PaymentTable';
-import { handleMortgageDataChange } from './utils';
+import React, { useState } from "react";
+import { HeaderFigures } from "./HeaderFigures";
+import { CalculatorControls } from "./CalculatorControls";
+import { PaymentGraph } from "./PaymentGraph";
+import { PaymentTable } from "./PaymentTable";
+import { handleMortgageDataChange } from "./utils";
 
 export const Calculator = () => {
-	const [depositAmount, setDepositAmount] = useState(72000);
-	const [purchasingHousePrice, setPurchasingHousePrice] = useState(285000);
-	const [mortgageTerm, setMortgageTerm] = useState(25);
-	const [interestRate, setInterestRate] = useState(1.8);
+  const [depositAmount, setDepositAmount] = useState(72000);
+  const [purchasingHousePrice, setPurchasingHousePrice] = useState(285000);
+  const [mortgageTerm, setMortgageTerm] = useState(30);
+  const [interestRate, setInterestRate] = useState(1.8);
 
-	//Set initial values for the whole mortgage term
-	const amountToBorrow = purchasingHousePrice - depositAmount;
-	const monthlyPayment = ((interestRate / 100 / 12) * amountToBorrow) / (1 - (Math.pow((1 + (interestRate / 100 / 12)),((0 - mortgageTerm) * 12))));
-	const totalRepaid = monthlyPayment * 12 * mortgageTerm;
-	const totalInterestPaid = totalRepaid - amountToBorrow;
+  //Set initial values for the whole mortgage term
+  const amountToBorrow = purchasingHousePrice - depositAmount;
+  const monthlyPayment =
+    ((interestRate / 100 / 12) * amountToBorrow) /
+    (1 - Math.pow(1 + interestRate / 100 / 12, (0 - mortgageTerm) * 12));
+  const totalRepaid = monthlyPayment * 12 * mortgageTerm;
+  const totalInterestPaid = totalRepaid - amountToBorrow;
 
-	const yearlyPayments = handleMortgageDataChange(amountToBorrow, mortgageTerm, interestRate, monthlyPayment);
+  const yearlyPayments = handleMortgageDataChange(
+    amountToBorrow,
+    mortgageTerm,
+    interestRate,
+    monthlyPayment
+  );
 
-	return(
-		<>
-			<HeaderFigures 
-				amountToBorrow={amountToBorrow}
-				monthlyPayment={monthlyPayment}
-				totalRepaid={totalRepaid}
-				totalInterestPaid={totalInterestPaid}
-				depositAmount={depositAmount}
-				purchasingHousePrice={purchasingHousePrice}
-			/>
-			<CalculatorControls 
-				depositAmount={depositAmount}
-				setDepositAmount={setDepositAmount}
-				purchasingHousePrice={purchasingHousePrice}
-				setPurchasingHousePrice={setPurchasingHousePrice}
-				mortgageTerm={mortgageTerm}
-				setMortgageTerm={setMortgageTerm}
-				interestRate={interestRate}
-				setInterestRate={setInterestRate}
-			/>
-			<PaymentGraph
-				mortgageTerm={mortgageTerm}
-				yearlyPayments={yearlyPayments}
-			/>
-			<PaymentTable
-				amountToBorrow={amountToBorrow}
-				yearlyPayments={yearlyPayments}
-			/>
-		</>
-	);
-
-}
+  return (
+    <>
+      <HeaderFigures
+        amountToBorrow={amountToBorrow}
+        monthlyPayment={monthlyPayment}
+        totalRepaid={totalRepaid}
+        totalInterestPaid={totalInterestPaid}
+        depositAmount={depositAmount}
+        purchasingHousePrice={purchasingHousePrice}
+      />
+      <CalculatorControls
+        depositAmount={depositAmount}
+        setDepositAmount={setDepositAmount}
+        purchasingHousePrice={purchasingHousePrice}
+        setPurchasingHousePrice={setPurchasingHousePrice}
+        mortgageTerm={mortgageTerm}
+        setMortgageTerm={setMortgageTerm}
+        interestRate={interestRate}
+        setInterestRate={setInterestRate}
+      />
+      <PaymentGraph
+        mortgageTerm={mortgageTerm}
+        yearlyPayments={yearlyPayments}
+      />
+      <PaymentTable
+        amountToBorrow={amountToBorrow}
+        yearlyPayments={yearlyPayments}
+      />
+    </>
+  );
+};
--- stayherelah/src/components/components/CalculatorControls.js
+++ stayherelah/src/components/components/CalculatorControls.js
@@ -1,75 +1,88 @@
-import React from 'react';
+import React from "react";
 
 const localeOptions = {
-	style:'currency',
-	currency:'USD',
-	minimumFractionDigits: 0,
-    maximumFractionDigits: 0
-}
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+};
 
-export const CalculatorControls = props => {
-	return(
-		<div className="grid__container">			
-			<div className="grid__item">
-				<span className="grid__item--header">{parseInt(props.purchasingHousePrice).toLocaleString('en-US', localeOptions)}</span>
-				<input 
-					type="range"
-					className="grid__item--range-slider"
-					id="purchasingHousePrice"  
-					min="50000" 
-					max="750000" 
-					step="1000" 
-					value={props.purchasingHousePrice} 
-					onChange={(event) => 
-						props.setPurchasingHousePrice(event.target.value)
-					} />
-				<label className="grid__item--label" htmlFor="purchasingHousePrice">Purchasing House Price</label>
-			</div>
-			<div className="grid__item">
-				<span className="grid__item--header">{parseInt(props.depositAmount).toLocaleString('en-US', localeOptions)}</span>
-				<input 
-					type="range"
-					className="grid__item--range-slider"
-					id="depositAmount" 
-					min="1000" 
-					max="150000" 
-					step="1000" 
-					value={props.depositAmount}
-         			onChange={(event) =>
-						props.setDepositAmount(event.target.value)
-         			} />
-				<label className="grid__item--label" htmlFor="points">Deposit</label>
-			</div>
-			<div className="grid__item">
-				<span className="grid__item--header">{props.mortgageTerm} Years</span>
-				<input 
-					type="range"
-					className="grid__item--range-slider"
-					id="mortgageTerm"  
-					min="5" 
-					max="35" 
-					step="1" 
-					value={props.mortgageTerm} 
-					onChange={(event) => 
-						props.setMortgageTerm(event.target.value)
-					} />
-				<label className="grid__item--label" htmlFor="mortgageTerm">Mortgage Term</label>
-			</div>
-			<div className="grid__item">
-				<span className="grid__item--header">{props.interestRate}%</span>
-				<input 
-					type="range"
-					className="grid__item--range-slider"
-					id="interestRate"  
-					min="0.1" 
-					max="8" 
-					step="0.1" 
-					value={props.interestRate} 
-					onChange={(event) => 
-						props.setInterestRate(event.target.value)
-					} />
-				<label className="grid__item--label" htmlFor="interestRate">Interest Rate</label>
-			</div>
-		</div>
-	);
-}
+export const CalculatorControls = (props) => {
+  return (
+    <div className="grid__container">
+      <div className="grid__item">
+        <span className="grid__item--header">
+          {parseInt(props.purchasingHousePrice).toLocaleString(
+            "en-US",
+            localeOptions
+          )}
+        </span>
+        <input
+          type="range"
+          className="grid__item--range-slider"
+          id="purchasingHousePrice"
+          min="50000"
+          max="1000000"
+          step="1000"
+          value={props.purchasingHousePrice}
+          onChange={(event) =>
+            props.setPurchasingHousePrice(event.target.value)
+          }
+        />
+        <label className="grid__item--label" htmlFor="purchasingHousePrice">
+          Purchasing House Price
+        </label>
+      </div>
+      <div className="grid__item">
+        <span className="grid__item--header">
+          {parseInt(props.depositAmount).toLocaleString("en-US", localeOptions)}
+        </span>
+        <input
+          type="range"
+          className="grid__item--range-slider"
+          id="depositAmount"
+          min="1000"
+          max="1000000"
+          step="1000"
+          value={props.depositAmount}
+          onChange={(event) => props.setDepositAmount(event.target.value)}
+        />
+        <label className="grid__item--label" htmlFor="points">
+          Deposit
+        </label>
+      </div>
+      <div className="grid__item">
+        <span className="grid__item--header">{props.mortgageTerm} Years</span>
+        <input
+          type="range"
+          className="grid__item--range-slider"
+          id="mortgageTerm"
+          min="5"
+          max="30"
+          step="1"
+          value={props.mortgageTerm}
+          onChange={(event) => props.setMortgageTerm(event.target.value)}
+        />
+        <label className="grid__item--label" htmlFor="mortgageTerm">
+          Mortgage Term
+        </label>
+      </div>
+      <div className="grid__item">
+        <span className="grid__item--header">{props.interestRate}%</span>
+        <input
+          type="range"
+          className="grid__item--range-slider"
+          id="interestRate"
+          min="0.1"
+          max="8"
+          step="0.1"
+          value={props.interestRate}
+          onChange={(event) => props.setInterestRate(event.target.value)}
+        />
+        <label className="grid__item--label" htmlFor="interestRate">
+          Interest Rate
+        </label>
+      </div>
+    </div>
+  );
+};
