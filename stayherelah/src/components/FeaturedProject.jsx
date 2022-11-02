7d439a2c097ee82b910aa94fb78952cef6acbbbf
lee-yu-xuan <102302118+lee-yu-xuan@users.noreply.github.com>
2022-11-02 21:42:42 +0800

    Picture when hover over mature estates

--- stayherelah/src/components/FeaturedProject.jsx
+++ stayherelah/src/components/FeaturedProject.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import mature from "../images/home/mature.png";
 
 const Project = styled.div`
   height: 80%;
@@ -12,8 +13,14 @@ const Project = styled.div`
   align-items: center;
   border-radius: 5px;
   background-color: #ffe7cc;
-
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  transition: 0.5s ease-out;
+  &:hover {
+    color: white;
+    background: url(${mature});
+    background-size: cover;
+    transition: 0.5s ease-out;
+  }
 `;
 
 const ProjectLink = styled(Link)`
